03f55ac5e02db149c595739a1bf732735ecf059e
Jon Q <hello@jonquach.com>
2020-01-17 15:27:34 -0500

    Fix posts selectors

--- src/pages/iteration2/gsdx/posts/posts-selectors.js
+++ src/pages/iteration2/gsdx/posts/posts-selectors.js
@@ -5,8 +5,9 @@ import { useStyleDataFromSource } from "../core";
 export const postsSelector = state => state.posts;
 
 const postSelector = id => {
-	return createSelector(postsSelector, posts =>
-		posts.find(post => post.id === id)
+	return createSelector(
+		postsSelector,
+		posts => posts.find(post => post.id === id) || {}
 	);
 };
 
@@ -14,7 +15,7 @@ const postBlocksSelector = id => {
 	return createSelector(postSelector(id), post => post.blocks || []);
 };
 
-const postBlockSelector = ({ id, blockId }) => {
+const postBlockSelector = ({ id, blockId } = {}) => {
 	return createSelector(
 		postBlocksSelector(id),
 		blocks => blocks.find(block => block.id === blockId) || {}
@@ -37,26 +38,26 @@ export const usePostBlocks = id => {
 	return blocks || [];
 };
 
-export const usePostBlock = ({ id, blockId }) => {
+export const usePostBlock = ({ id, blockId } = {}) => {
 	const block = useSelector(postBlockSelector({ id, blockId }));
 
 	return block || {};
 };
 
-export const usePostBlockStyles = ({ id, blockId }) => {
+export const usePostBlockStyles = ({ id, blockId } = {}) => {
 	const block = usePostBlock({ id, blockId });
 
 	return block.styles || {};
 };
 
-export const usePostBlockStylesData = ({ id, blockId }) => {
+export const usePostBlockStylesData = ({ id, blockId } = {}) => {
 	const blockStyles = usePostBlockStyles({ id, blockId });
 	const styleData = useStyleDataFromSource(blockStyles);
 
 	return styleData;
 };
 
-export const usePostBlockStylesCssVariables = ({ id, blockId }) => {
+export const usePostBlockStylesCssVariables = ({ id, blockId } = {}) => {
 	const styleData = usePostBlockStylesData({ id, blockId });
 
 	return styleData.variables;
